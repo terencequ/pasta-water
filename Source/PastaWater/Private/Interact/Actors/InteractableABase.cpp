808d770e752672732a0950196dce1a7ad480c5b4
Terence Qu <therealterencequ@gmail.com>
2021-09-18 13:22:50 +1200

    Code formatting cleanup

--- Source/PastaWater/Private/Interact/Actors/InteractableABase.cpp
+++ Source/PastaWater/Private/Interact/Actors/InteractableABase.cpp
@@ -1,6 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Interact/Actors/InteractableABase.h"
 #include "Helpers/DebugHelpers.h"
 
--- Source/PastaWater/Public/Inventory/ActorComponents/PlayerInventoryAC.h
+++ Source/PastaWater/Public/Inventory/ActorComponents/PlayerInventoryAC.h
@@ -47,33 +47,23 @@ public:
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
 	                           FActorComponentTickFunction* ThisTickFunction) override;
 
-	UFUNCTION(BlueprintCallable, Category="Player Inventory Single Slot")
 	virtual FItemStack GetItemStackAtIndex_Implementation(const int Index) const override;
 	
-	UFUNCTION(BlueprintCallable, Category="Player Inventory Single Slot")
 	virtual void SetItemStackAtIndex_Implementation(const int Index, const FItemStack ItemStack) override;
 	
-	UFUNCTION(BlueprintCallable, Category="Player Inventory Properties")
 	virtual int GetContainerSize_Implementation() const override;
 	
-	UFUNCTION(BlueprintCallable, Category="Player Inventory Actions")
 	virtual void SwapItemStacks_Implementation(const int FirstIndex, const int SecondIndex) override;
 	
-	UFUNCTION(BlueprintCallable, Category="Player Inventory Actions")
 	virtual bool CanInsertItemStack_Implementation(const FItemStack ItemStack) const override;
-
-	UFUNCTION(BlueprintCallable, Category="Player Inventory Actions")
+	
 	virtual FItemStack InsertItemStack_Implementation(const FItemStack ItemStack) override;
-
-	UFUNCTION(BlueprintCallable, Category="Player Inventory Actions")
+	
 	virtual bool CanRemoveItemStack_Implementation(const FItemStack ItemStack) const override;
-
-	UFUNCTION(BlueprintCallable, Category="Player Inventory Actions")
+	
 	virtual FItemStack RemoveItemStack_Implementation(const FItemStack ItemStack) override;
-
-	UFUNCTION(BlueprintCallable, Category="Player Inventory All Slots")
+	
 	virtual TArray<FItemStack> GetAllItemStacks_Implementation() const override;
-
-	UFUNCTION(BlueprintCallable, Category="Player Inventory All Slots")
+	
 	virtual TArray<FItemStack> RemoveAllItemStacks_Implementation() override;
 };
