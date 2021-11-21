a31cf01da8d7eef18f95d62d63b539247434b0a7
Terence Qu <therealterencequ@gmail.com>
2021-11-21 22:03:00 +1300

    Refactoring inventory widget class logic to a seperate base class

--- Source/PastaWater/Public/Core/Inventory/Models/ItemStack.h
+++ Source/PastaWater/Public/Core/Inventory/Models/ItemStack.h
@@ -3,7 +3,6 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Item.h"
 #include "ItemStack.generated.h"
 
 /**
---
+++ Source/PastaWater/Public/Player/Widgets/PlayerHotbarWidget.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InventoryWidget.h"
+#include "PlayerHotbarWidget.generated.h"
+
+/**
+ * Widget which displays a player's hotbar and keeps track of the active item.
+ */
+UCLASS()
+class PASTAWATER_API UPlayerHotbarWidget : public UInventoryWidget
+{
+	GENERATED_BODY()
+	
+protected:
+	int Columns = 10;
+	int Rows = 1;
+
+public:
+	static UPlayerHotbarWidget* Create(TSubclassOf<UPlayerHotbarWidget> PlayerInventoryWidgetClass,
+	                            APastaWaterPlayerControllerBase* OwningPlayerController,
+	                            UPlayerInventoryAC* OwningPlayerInventoryAC);
+};
